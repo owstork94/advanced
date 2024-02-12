77a40b072d2646e9aa67a1c5ca21f263c1c8a15d
owsto <owstork94@gmail.com>
2024-02-12 21:22:07 +0900

    .

--- src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -1,11 +1,43 @@
 package hello.advanced.trace.logtrace;
 
+import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
 
+import static com.sun.jndi.ldap.LdapPoolManager.trace;
+
+@Slf4j
 public class FieldLogTrace implements LogTrace{
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    TraceId traceIdholder;
     @Override
     public TraceStatus begin(String message) {
-        return null;
+        syncTraceId();
+        TraceId traceId = traceIdholder;
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}",traceId.getId(), addSpace(START_PREFIX,traceId.getLevel()),message);
+
+        return new TraceStatus(traceId,startTimeMs,message);
+    }
+
+    private Object addSpace(String prefix, int level) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            stringBuilder.append((i == level -1) ? "|"+prefix : "|  ");
+
+        }
+        return stringBuilder.toString();
+    }
+
+    private void syncTraceId() {
+        if (traceIdholder == null){
+            traceIdholder = new TraceId();
+        }else {
+            traceIdholder = traceIdholder.createNextId();
+        }
     }
 
     @Override
